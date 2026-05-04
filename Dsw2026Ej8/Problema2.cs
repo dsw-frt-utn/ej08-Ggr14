e8f41673b86f3c531957833e2c4bd3bb6df5294c
Gonzalo <gonzaloruiz@gmail.com>
2026-05-04 19:57:15 -0300

    commit del problema 2

---
+++ Dsw2026Ej8/Problema2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema2
+    {
+        public String CrearResumenVenta(long productCode, String productDescription, int quantity, decimal unitPrice)
+        {
+            decimal total;
+            if (quantity <= 0) {
+                total = 0;
+            }
+            else
+            {
+                total = quantity * unitPrice;
+            }
+            var aux = new
+            {
+                Code = productCode,
+                Description = productDescription,
+                Quantity = quantity,
+                Total = total
+            };
+            return $"{aux.Code}-{aux.Description}-{aux.Total}";
+
+        }
+    }
+}
