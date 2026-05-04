855a4ad96a253a4bac70d568e2ffac01dbd617ee
Gonzalo <gonzaloruiz@gmail.com>
2026-05-04 19:52:53 -0300

    Problema 1 agregado

---
+++ Dsw2026Ej8/ProductHelper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public String ObtenerEtiquetaProducto(long code, String descripcion, decimal price)
+        {
+            String precioformat = FormatoMoneda(price);
+            return $"[{code}] {descripcion} - {precioformat}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/ProductHelperV2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal partial class ProductHelper
+    {
+        public string FormatoMoneda(decimal price)
+        {
+            return "$" + price.ToString();
+        }
+    }
+}
