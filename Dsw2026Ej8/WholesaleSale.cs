cb710056e7fa125002bbe8b9b567b601a0341e66
Gonzalo <gonzaloruiz@gmail.com>
2026-05-04 20:25:20 -0300

    Commit del problema 5

---
+++ Dsw2026Ej8/WholesaleSale.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class WholesaleSale : Sale
+    {
+        public WholesaleSale(decimal importe) : base(importe) { }
+        public override decimal CalculateTotal()
+        {
+            decimal descuento = Importe * 0.10m;
+            return Importe - descuento;
+        }
+
+    }
+}
