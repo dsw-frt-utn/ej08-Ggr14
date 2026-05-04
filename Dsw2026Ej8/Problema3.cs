1318fdff21c1484d621886dbd8dea367bcd327b4
Gonzalo <gonzaloruiz@gmail.com>
2026-05-04 19:58:19 -0300

    Commit del problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Problema3
+    {
+        public String CompararCopias(int originalValue, Product product)
+        {
+            int valor=originalValue;
+            valor++;
+            Product productvalor= product;
+            productvalor.Modificar("modificado");
+            return $"{originalValue}-{valor}-{productvalor.Descripcion}";
+        }
+    }
+}
---
+++ Dsw2026Ej8/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    internal class Product
+    {
+        public string Descripcion { get; set; }
+        public Product (string descripcion)
+        {
+            Descripcion=descripcion;
+        }
+        public void Modificar (string nuevaDescripcion)
+        {
+            Descripcion = nuevaDescripcion; 
+        }
+    }
+}
